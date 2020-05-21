99884e3d46f4afa0fbcb6b937696554e56e3e995
zidan5 <tmabuza2@gmail.com>
2020-05-21 14:37:25 +0200

    generic class and method combined in single class

--- src/GenericClass.java
+++ src/GenericClass.java
@@ -1,22 +1,36 @@
 public class GenericClass<T> {
-        private T variable;
+    private T variable;
 
-        public void set(T input){  // Setter
-            variable = input;
-        }
-        public void get(){  // Getter
-            System.out.println(variable);
-        }
+    public void set(T input) {  // Setter
+        variable = input;
+    }
 
-    public static void main(String[] Args){
+    public void get() {  // Getter
+        System.out.println(variable);
+    }
+
+    public static <E> void printArrayContent(E[] inputArray) {
+        for (E element : inputArray) {
+            System.out.print(element + " ");
+        }
+    }
+        public static void main (String[]Args){
 
             GenericClass gen = new GenericClass<Integer>(); //Using Integers in Generic class
             gen.set(3);
             gen.get();
 
-            GenericClass gen1 =new GenericClass<String>(); //Using String in Generic class
+            GenericClass gen1 = new GenericClass<String>(); //Using String in Generic class
             gen1.set("Generic class");
             gen1.get();
-        
+
+            Integer[] IntArray = {2, 4, 6, 8, 10};
+            String[] stringArray = {"Two", "Four", "Six", "Eight", "Ten"};
+
+            printArrayContent(stringArray); //Printing String elements using printArrayContent method
+            System.out.println();
+            printArrayContent(IntArray); //Printing integer elements using printArrayContent method
+
+        }
     }
-}
+
